440cf93505d13182673cd3ace0c083dc1cf9f4b9
Meghna Kar <meghnakar499415@gmail.com>
2026-01-13 01:03:31 +0530

    Refactor UART testbench for improved timing and data handling

--- uart_tb/tb_uart_top.v
+++ uart_tb/tb_uart_top.v
@@ -1,7 +1,7 @@
 `timescale 1ns / 1ps
 module tb_uart_top();
 reg clk=0;
-always #10 clk =~clk;
+always #0.01 clk =~clk;
 
 reg rst,tx_start;
 reg [7:0]tx_data;
@@ -10,21 +10,36 @@ wire rx_done;
 
 uart_top dut3(.clk(clk),.rst(rst),.tx_start(tx_start),.tx_data(tx_data),.rx_data(rx_data),.rx_done(rx_done));
 
-initial
+task send_byte(input [7:0] b);
+begin
+rst=1;
+@(posedge clk);
+rst=0;
+@(posedge clk);
+tx_data= b;
+tx_start=1'b1;
+@(posedge clk);
+tx_start=1'b0;
+wait(rx_done == 1'b1);
+$display("TX DATA=%d ,RX DATA = %d",tx_data,rx_data);
+repeat(20)@(posedge clk);
+end 
+endtask 
+
+initial 
 begin
 rst=1;
 tx_start=0;
 tx_data=8'h00;
-repeat(10) @(posedge clk);
+repeat(20) @(posedge clk);
 rst=0;
 @(posedge clk);
-tx_data=8'h55;
-tx_start=1'b1;
-@(posedge clk);
-tx_start=1'b0;
-wait(rx_done);
-$display("RX DATA = %h",rx_data);
-#100;
+send_byte(8'h00);
+send_byte(8'h5E);
+send_byte(8'hA3);
+send_byte(8'hff);
+send_byte(8'hC7);
+repeat(50)@(posedge clk);
 $finish;
 end
 endmodule
