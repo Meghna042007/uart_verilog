36f7c49e1093ab4f786f554fe4fecf649ce48139
Meghna Kar <meghnakar499415@gmail.com>
2026-01-13 01:00:54 +0530

    Update uart_top.v

--- uart_rtl/uart_top.v
+++ uart_rtl/uart_top.v
@@ -1,16 +1,20 @@
-`timescale 1ns / 1ps
+ `timescale 1ns / 1ps 
 module uart_top(
 input clk,rst,tx_start,
 input [7:0]tx_data,
 output [7:0]rx_data,
 output rx_done);
 
+wire tx;
 wire uart_line;
 wire tx_busy;
 
 uart_tx tx_inst(
-.clk(clk),.rst(rst),.tx_start(tx_start),.tx_data(tx_data),.tx(uart_line),.tx_busy(tx_busy));
+.clk(clk),.rst(rst),.tx_start(tx_start),.tx_data(tx_data),.tx(tx),.tx_busy(tx_busy));
+
+assign uart_line = tx_busy?tx:1'b1;
 
 uart_rx rx_inst(
 .clk(clk),.rst(rst),.rx(uart_line),.rx_data(rx_data),.rx_done(rx_done));
+
 endmodule
